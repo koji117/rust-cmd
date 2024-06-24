6a894610760342b915dc16f89ff1aaa71ff973a8
Koji Saruya <koji.saruya@woven-planet.global>
2024-06-24 14:25:29 +0900

    Work: jun 23 2024

--- findr/src/main.rs
+++ findr/src/main.rs
@@ -1,6 +1,8 @@
+use anyhow::Result;
 use clap::builder::PossibleValue;
 use clap::{ArgAction, Parser, ValueEnum};
 use regex::Regex;
+use walkdir::WalkDir;
 
 #[derive(Debug, Eq, PartialEq, Clone)]
 enum EntryType {
@@ -27,10 +29,11 @@ impl ValueEnum for EntryType {
 #[command(author, version, about)]
 /// Rust version of `find`
 struct Args {
-    #[arg(value_name = "PATH", default_value = ".", last = true)]
+    // Search path(s)
+    #[arg(value_name = "PATH", default_value = ".")]
     paths: Vec<String>,
 
-    /// Name
+    /// File names
     #[arg(
         value_name = "NAME",
         short('n'), long("name"),
@@ -40,16 +43,56 @@ struct Args {
     )]
     names: Vec<Regex>,
 
+    // Entry types, FILE and(or) LINK
     #[arg(
         value_name = "TYPE",
         short('t'), long("type"),
         value_parser = clap::value_parser!(EntryType), // Parse input values to EntryType (File, Dir or Link)
+        action(ArgAction::Append),
         num_args(0..) // // Appends each occurrence to the types vector
     )]
     entry_types: Vec<EntryType>,
 }
 
 fn main() {
-    let args = Args::parse();
-    println!("{:?}", args);
+    let execution_result = run(Args::parse());
+
+    match execution_result {
+        Err(e) => {
+            eprintln!("{e}");
+            std::process::exit(1);
+        }
+        Ok(()) => {
+            // println!("BIG SUCCESS")
+        }
+    }
+}
+
+fn run(args: Args) -> Result<()> {
+    for path in &args.paths {
+        // println!("{:?}", args);
+        for entry in WalkDir::new(path) {
+            match entry {
+                Err(e) => eprintln!("Error: {e}"),
+                Ok(entry) => {
+                    if args.entry_types.is_empty() {
+                        println!("{}", entry.path().display());
+                    }
+                    if entry.path().is_dir() && args.entry_types.contains(&EntryType::Dir) {
+                        let path_str = entry.path().to_str().unwrap().to_string();
+                        println!("{}", path_str);
+                    }
+                    if entry.path().is_file() && args.entry_types.contains(&EntryType::File) {
+                        let path_str = entry.path().to_str().unwrap().to_string();
+                        println!("{}", path_str);
+                    }
+                    if entry.path().is_symlink() && args.entry_types.contains(&EntryType::Link) {
+                        let path_str = entry.path().to_str().unwrap().to_string();
+                        println!("{}", path_str);
+                    }
+                }
+            }
+        }
+    }
+    Ok(())
 }
